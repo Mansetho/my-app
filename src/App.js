31f949cd9e0ab039a6d3c8c1db07abaa5879df4d
Mansetho <92010117+Mansetho@users.noreply.github.com>
2022-12-20 21:27:45 +0300

    students

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-import Students from './components/Students';
+import "./App.css";
+import Students from "./components/Students";
 
 function App() {
+  let details = [
+    {
+      name: "Seth",
+      fullName: "Seth Okey",
+      CoursePersued: "Machine learning",
+      age: "24",
+    },
+    {
+      name: "silva",
+      fullName: "thiago silva",
+      CoursePersued: "data science",
+      age: "32",
+    },
+    {
+      name: "Allison",
+      fullName: "Allison Becker",
+      CoursePersued: "full stack development",
+      age: "26",
+    },
+    {
+      name: "Naymar",
+      fullName: "Naymar Junior",
+      CoursePersued: "django python",
+      age: "30",
+    },
+    {
+      name: "Travis",
+      fullName: "Travis Scotts",
+      CoursePersued: "Laravel",
+      age: "28",
+    },
+    {
+      name: "Alex",
+      fullName: "Alexander Brooks",
+      CoursePersued: "Ethical Hacking",
+      age: "30",
+    },
+  ];
   return (
     <div className="App">
-       <Students name= 'Seth' fullName='Seth okeyo' CoursePersued='Machine learning' age='34'/>
-      <Students name= 'silva'  fullName='thiago silva' CoursePersued='data science' age='32' />
-      <Students name= 'Allison'  fullName='Allison Becker' CoursePersued='full stack development' age=' 26'/>
-      <Students name = 'Naymar' fullName='Naymar Junior' CoursePersued='django python' age="30"/> 
-    <Students name='Travis' fullName='Travis Scotts' CoursePersued='Laravel' age='28'/>
-    <Students name='Alex' fullName='Alexander Brooks' CoursePersued='Ethical Hacking' age='30'/>
-    <Students name='James' fullName='Reece James' CoursePersued='react Native' age='25'/>
-    <Students name='Thomas' fullName='Thomas Tuchel' CoursePersued='Spring Java' age='40'/>
-    <Students name='Zlatan' fullName='Zlatan Ibrahimovich' CoursePersued='Electronics' age='34'/>
-    <Students name='Diogo' fullName='diogo Costa' CoursePersued='Robotics' age='45'/>
-    
-    
-
-
-
-
-     
+      {details.map((data) => {
+        return (
+          <Students
+            name={data.name}
+            fullName={data.fullName}
+            CoursePersued={data.CoursePersued}
+            age={data.age}
+          />
+        );
+      })}
     </div>
   );
 }
